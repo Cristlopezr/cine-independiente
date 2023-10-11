3fbc1167d09c02cfe6aa298ff78b69b63f58bf83
Cristian López <cristianlopezr.931@gmail.com>
2023-10-11 14:49:15 -0300

    Responsive header linear-gradient background

--- src/components/cine/moviePage/Header.tsx
+++ src/components/cine/moviePage/Header.tsx
@@ -11,6 +11,19 @@ interface HeaderProps {
 	isCarousel?: boolean;
 }
 
+const gradientStyle = {
+	backgroundImage: `linear-gradient(
+	  hsla(224, 71.4%, 4.1%, 0) 0%, hsla(224, 71.4%, 4.1%, 0) 49.02%, 
+	  hsla(224, 71.4%, 4.1%, 0.009) 52.42%, hsla(224, 71.4%, 4.1%, 0.036) 55.82%, 
+	  hsla(224, 71.4%, 4.1%, 0.082) 59.22%, hsla(224, 71.4%, 4.1%, 0.15) 62.62%, 
+	  hsla(224, 71.4%, 4.1%, 0.23) 66.02%, hsla(224, 71.4%, 4.1%, 0.332) 69.41%, 
+	  hsla(224, 71.4%, 4.1%, 0.443) 72.81%, hsla(224, 71.4%, 4.1%, 0.557) 76.21%, 
+	  hsla(224, 71.4%, 4.1%, 0.668) 79.61%, hsla(224, 71.4%, 4.1%, 0.77) 83.01%, 
+	  hsla(224, 71.4%, 4.1%, 0.85) 86.41%, hsla(224, 71.4%, 4.1%, 0.918) 89.8%, 
+	  hsla(224, 71.4%, 4.1%, 0.964) 93.2%, hsla(224, 71.4%, 4.1%, 0.991) 96.6%, 
+	  hsla(224, 71.4%, 4.1%, 0) 100%)`,
+};
+
 export const Header = ({ movie, onClickPlay, isCarousel }: HeaderProps) => {
 	const navigate = useNavigate();
 
@@ -20,6 +33,9 @@ export const Header = ({ movie, onClickPlay, isCarousel }: HeaderProps) => {
 
 	return (
 		<section className='font-semibold main-slider-container relative overflow-hidden'>
+			<div className='absolute w-full h-[700px] -bottom-10' style={gradientStyle}></div>
+			<div className='absolute w-full h-[600px] -bottom-10' style={gradientStyle}></div>
+			<div className='absolute w-full h-[600px] -bottom-10' style={gradientStyle}></div>
 			<section className='absolute flex flex-col gap-3 bottom-10 font-semibold z-10 px-14 w-full'>
 				<p className='text-3xl sm:text-5xl md:text-6xl lg:text-7xl'>{movie.title}</p>
 				<p className='text-gray-300 font-thin'>{movie.productionYear}</p>
--- src/components/cine/moviePage/HeaderImage.tsx
+++ src/components/cine/moviePage/HeaderImage.tsx
@@ -2,9 +2,6 @@ export const HeaderImage = ({ imageUrl }: { imageUrl: string }) => {
 	return (
 		<div className='w-full'>
 			<img className='w-full aspect-[16/9] object-cover' src={imageUrl} alt='Portada de la película' />
-			<div className='absolute bg-gradient-to-l z-0 from-transparent from-[80%] to-background/30 top-[0%] -bottom-10 left-0 right-0'></div>
-			<div className='absolute bg-gradient-to-b from-transparent from-0% to-background top-[50%] bottom-0 left-0 right-0'></div>
-			<div className='absolute bg-gradient-to-b from-transparent from-0% to-background top-[70%] bottom-0 left-0 right-0'></div>
 		</div>
 	);
 };
