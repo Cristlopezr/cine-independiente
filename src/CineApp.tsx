dbf3c05019895c27f2b9c8e8810a87110a830d5b
Cristian López <cristianlopezr.931@gmail.com>
2023-11-02 13:56:59 -0300

    video added

--- src/CineApp.tsx
+++ src/CineApp.tsx
@@ -1,11 +1,6 @@
 import './App.css';
-import { ThemeProvider } from './components';
 import { AppRouter } from './router';
 
 export const CineApp = () => {
-	return (
-		<ThemeProvider>
-			<AppRouter />
-		</ThemeProvider>
-	);
+	return <AppRouter />;
 };
--- src/cine/pages/Room.tsx
+++ src/cine/pages/Room.tsx
@@ -1,3 +1,4 @@
+import { VideoElWatchParty } from '@/components/cine/roomPage';
 import { Avatar, AvatarFallback, AvatarImage, Button, Loading, Separator } from '@/components/ui';
 import { formatMovieTime } from '@/helpers';
 import { useAuthStore } from '@/hooks';
@@ -19,17 +20,22 @@ const pageText = {
 };
 
 type RoomStatus = 'playing' | 'waiting';
+type MovieState = 'started' | 'not-started';
 export const Room = () => {
 	const { movie_id, room_id } = useParams();
 	const { user } = useAuthStore();
 	const [participants, setParticipants] = useState<User[]>([]);
+	const [movieState, setMovieState] = useState<MovieState>('not-started');
 	const [socket, setSocket] = useState<Socket>();
-	const [roomStatus, setRoomStatus] = useState<RoomStatus | null>(null);
+	const [roomStatus, setRoomStatus] = useState<RoomStatus>('waiting');
 	const navigate = useNavigate();
 	const { data, isError, isFetching } = useGetMovieQuery(movie_id!);
+	const [playerState, setPlayerState] = useState({ viewingTime: 0 });
 
 	useEffect(() => {
-		const socket = io('http://localhost:8081');
+		const socket = io('http://localhost:8081', {
+			transports: ['websocket'],
+		});
 		setSocket(socket);
 		socket.on('connect', () => {
 			socket.emit('CLIENT:user-joined-room', { room_id, user: { ...user } });
@@ -45,9 +51,9 @@ export const Room = () => {
 			});
 		});
 
-		socket.on('SERVER:started-movie', ({ room_status }) => {
+		socket.on('SERVER:started-movie', ({ movie_state }) => {
 			console.log('asd');
-			setRoomStatus(room_status);
+			setMovieState(movie_state);
 		});
 
 		socket.on('SERVER:room-status', ({ room_status }) => {
@@ -65,7 +71,7 @@ export const Room = () => {
 		return () => {
 			socket.off('SERVER:started-movie');
 			socket.off('SERVER:participants');
-			socket.off('SERVER:room-status');
+			/* 	socket.off('SERVER:room-status'); */
 			socket.off('connect');
 			socket.off('disconnect');
 			socket.close();
@@ -100,128 +106,91 @@ export const Room = () => {
 
 	const onClickPlay = () => {
 		console.log('play');
-		socket?.emit('CLIENT:start-movie', { room_id, room_status: 'playing' });
+		socket?.emit('CLIENT:start-movie', { room_id, room_status: 'playing', movie_state: 'started' });
 		//!Change room status and send room status
 	};
 
 	const { movie } = data!;
 
+	const { viewingTime } = playerState;
+
 	console.log({ roomStatus });
 	return (
-		<div className='mt-[100px] px-5 sm:px-10'>
-			<h1 className='text-2xl sm:px-5 font-semibold'>{pageText.pageTitle}</h1>
-			<Separator className='my-5' />
-			<div className='grid grid-cols-1 gap-10 lg:grid-cols-2'>
-				<div className='flex flex-col w-full md:max-w-[600px] sm:px-5 gap-10'>
-					<h2 className='text-2xl'>{movie.title}</h2>
-					<div className='w-full rounded-md overflow-hidden'>
-						<img src={movie.imageUrl} className='w-full' alt='Imagen película' />
-					</div>
-					<div className='flex flex-col gap-5'>
-						<p>{movie.synopsis}</p>
-						<div className='flex text-xs md:text-base items-center gap-5 text-white/70 font-normal tracking-widest'>
-							<p className='drop-shadow-[0_5px_5px_hsla(224,71.4%,4.1%,1)]'>
-								{formatMovieTime(movie.duration)} MIN
-							</p>
-							<p className='drop-shadow-[0_5px_5px_hsla(224,71.4%,4.1%,1)]'>
-								{movie.productionYear}
-							</p>
+		<>
+			{movieState === 'not-started' ? (
+				<div className='mt-[100px] px-5 sm:px-10'>
+					<h1 className='text-2xl sm:px-5 font-semibold'>{pageText.pageTitle}</h1>
+					<Separator className='my-5' />
+					<div className='grid grid-cols-1 gap-10 lg:grid-cols-2'>
+						<div className='flex flex-col w-full md:max-w-[600px] sm:px-5 gap-10'>
+							<h2 className='text-2xl'>{movie.title}</h2>
+							<div className='w-full rounded-md overflow-hidden'>
+								<img src={movie.imageUrl} className='w-full' alt='Imagen película' />
+							</div>
+							<div className='flex flex-col gap-5'>
+								<p>{movie.synopsis}</p>
+								<div className='flex text-xs md:text-base items-center gap-5 text-white/70 font-normal tracking-widest'>
+									<p className='drop-shadow-[0_5px_5px_hsla(224,71.4%,4.1%,1)]'>
+										{formatMovieTime(movie.duration)} MIN
+									</p>
+									<p className='drop-shadow-[0_5px_5px_hsla(224,71.4%,4.1%,1)]'>
+										{movie.productionYear}
+									</p>
+								</div>
+								<div className='flex flex-col gap-5'>
+									<ul className='flex text-xs flex-wrap gap-5 sm:text-sm'>
+										{movie.genres.map(({ name, genre_id }) => (
+											<li key={genre_id} className='flex items-center gap-2'>
+												<div className='w-1 h-1 bg-white/80 rounded-full'></div>
+												{name}
+											</li>
+										))}
+									</ul>
+								</div>
+							</div>
+							<Button onClick={onLeaveWatchParty} className='w-fit'>
+								{pageText.leaveGroupButton}
+							</Button>
 						</div>
-						<div className='flex flex-col gap-5'>
-							<ul className='flex text-xs flex-wrap gap-5 sm:text-sm'>
-								{movie.genres.map(({ name, genre_id }) => (
-									<li key={genre_id} className='flex items-center gap-2'>
-										<div className='w-1 h-1 bg-white/80 rounded-full'></div>
-										{name}
-									</li>
+						<div className='flex flex-col items-center gap-10 lg:pt-[100px]'>
+							{participants.length === MAX_PARTICIPANTS && <div>{pageText.roomComplete}</div>}
+							{roomStatus === 'playing' && (
+								<div>La película ya empezó, unete haciendo clic en empezar a ver.</div>
+							)}
+							<div className='flex gap-10 items-center'>
+								{participants.map(({ user_id, name, avatarUrl }, i) => (
+									<span
+										key={user_id}
+										className={`flex flex-col items-center ${
+											i === 0 ? 'order-2' : i === 1 ? 'order-1' : 'order-3'
+										}`}
+									>
+										<Avatar
+											className={`${
+												i === 0 ? 'w-[10rem] h-[10rem]' : 'w-[8rem] h-[8rem]'
+											} border-4 border-border-second`}
+										>
+											<AvatarImage src={avatarUrl ? avatarUrl : undefined} />
+											<AvatarFallback className='text-4xl'>
+												{name.slice(0, 1)}
+											</AvatarFallback>
+										</Avatar>
+										<p className='hidden md:block text-white'>{name}</p>
+									</span>
 								))}
-							</ul>
+							</div>
+							<div className='flex flex-col gap-1 items-center w-full'>
+								<Button onClick={onClickPlay} className='w-fit'>
+									{pageText.startWatchingButton}
+								</Button>
+								<p className='text-xl font-semibold'>{pageText.inviteText}</p>
+							</div>
 						</div>
 					</div>
-					<Button onClick={onLeaveWatchParty} className='w-fit'>
-						{pageText.leaveGroupButton}
-					</Button>
 				</div>
-				<div className='flex flex-col items-center gap-10 lg:pt-[100px]'>
-					{participants.length === MAX_PARTICIPANTS && <div>{pageText.roomComplete}</div>}
-					<div className='flex gap-10 items-center'>
-						{participants.map(({ user_id, name, avatarUrl }, i) => (
-							<span
-								key={user_id}
-								className={`flex flex-col items-center ${
-									i === 0 ? 'order-2' : i === 1 ? 'order-1' : 'order-3'
-								}`}
-							>
-								<Avatar
-									className={`${
-										i === 0 ? 'w-[10rem] h-[10rem]' : 'w-[8rem] h-[8rem]'
-									} border-4 border-border-second`}
-								>
-									<AvatarImage src={avatarUrl ? avatarUrl : undefined} />
-									<AvatarFallback className='text-4xl'>{name.slice(0, 1)}</AvatarFallback>
-								</Avatar>
-								<p className='hidden md:block text-white'>{name}</p>
-							</span>
-						))}
-					</div>
-					<div className='flex flex-col gap-1 items-center w-full'>
-						<Button onClick={onClickPlay} className='w-fit'>
-							{pageText.startWatchingButton}
-						</Button>
-						<p className='text-xl font-semibold'>{pageText.inviteText}</p>
-					</div>
-					{/* 	<div className='grid grid-cols-2 relative h-full w-full grid-rows-2'>
-						{participants?.map(({ user_id, name, avatarUrl }, i) => {
-							return (
-								<span
-									key={user_id}
-									className={`flex flex-col gap-2 items-center ${
-										i === 0
-											? 'col-span-2 row-start-1 place-self-center row-span-2'
-											: 'row-start-2'
-									} ${i === 0 && participants.length > 1 ? 'absolute' : ''}`}
-								>
-									<Avatar
-										className={`${
-											i === 0 ? 'w-[10rem] h-[10rem]' : 'w-[8rem] h-[8rem]'
-										} border-4 border-border-second`}
-									>
-										<AvatarImage src={avatarUrl ? avatarUrl : undefined} />
-										<AvatarFallback className='text-4xl'>
-											{name.slice(0, 1)}
-										</AvatarFallback>
-									</Avatar>
-									<p className='hidden md:block text-white'>{name}</p>
-								</span>
-							);
-						})}
-					</div> */}
-					{/* <div className='relative bg-red-500 w-full h-full'>
-						{participants?.map(({ user_id, name, avatarUrl }, i) => {
-							return (
-								<span
-									key={user_id}
-									className={`${
-										i === 0 ? 'left-1/2 -translate-x-1/2 -translate-y-1/2 top-1/2' : ''
-									} flex flex-col gap-2 items-center absolute`}
-								>
-									<Avatar
-										className={`${
-											i === 0 ? 'w-[10rem] h-[10rem]' : 'w-[8rem] h-[8rem]'
-										} border-4 border-border-second`}
-									>
-										<AvatarImage src={avatarUrl ? avatarUrl : undefined} />
-										<AvatarFallback className='text-4xl'>
-											{name.slice(0, 1)}
-										</AvatarFallback>
-									</Avatar>
-									<p className='hidden md:block text-white'>{name}</p>
-								</span>
-							);
-						})}
-					</div> */}
-				</div>
-			</div>
-		</div>
+			) : (
+				<VideoElWatchParty movie={movie} viewingTime={viewingTime} />
+			)}
+		</>
 	);
 };
